68f4593b97bcb473e4d29511f6921b083f82dc76
dartk <dartk@users.noreply.github.com>
2022-08-07 16:56:58 +0500

    Sample project updated

--- Sample/Tests.cs
+++ Sample/Tests.cs
@@ -9,10 +9,7 @@ public class Tests {
     
     [TestMethod]
     public void ClassFromScribanTest() {
-        Assert.AreEqual(0, ClassFromScriban.Value0);
-        Assert.AreEqual(1, ClassFromScriban.Value1);
-        Assert.AreEqual(2, ClassFromScriban.Value2);
-        Assert.AreEqual(3, ClassFromScriban.Value3);
+        Assert.AreEqual(42, GeneratedByScriban.GlobalValue);
     }
     
 }
