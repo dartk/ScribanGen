d578855426ec7e3bfcf37767c29adeb66597dd98
dartk <dartk@users.noreply.github.com>
2022-08-07 14:17:28 +0500

    Generator and sample projects added

---
+++ CSharpScribanGenerator/ScribanGenerator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+
+
+namespace ScribanGenerator;
+
+
+[Generator]
+public class ScribanGenerator : ISourceGenerator {
+
+    public void Initialize(GeneratorInitializationContext context) {
+    }
+
+
+    public void Execute(GeneratorExecutionContext context) {
+        foreach (var file in context.AdditionalFiles) {
+            if (Path.GetExtension(file.Path) != ".scriban" || Path.GetFileName(file.Path).First() == '_') {
+                continue;
+            }
+
+            var source = this._scribanRenderer.Render(Path.GetFullPath(file.Path));
+            context.AddSource(Path.GetFileNameWithoutExtension(file.Path), source);
+        }
+    }
+
+
+    private readonly ScribanRenderer _scribanRenderer = new ();
+
+}
---
+++ CSharpScribanGenerator/ScribanRenderer.cs
@@ -0,0 +1,56 @@
+﻿using Scriban;
+using Scriban.Parsing;
+using Scriban.Runtime;
+
+
+namespace ScribanGenerator;
+
+
+public class ScribanRenderer {
+
+    public ScribanRenderer() {
+        this._templateLoader = new DirectoryTemplateLoader();
+        this._templateContext = new TemplateContext { TemplateLoader = this._templateLoader };
+    }
+
+
+    public string Render(string fullPath) {
+        var template = Template.Parse(File.ReadAllText(fullPath), fullPath);
+        this._templateLoader.TemplateDirectory = Path.GetDirectoryName(fullPath);
+        return template.Render(this._templateContext);
+    }
+        
+
+    private readonly TemplateContext _templateContext;
+    private readonly DirectoryTemplateLoader _templateLoader;
+    
+
+    /// <summary>
+    /// Loads templates from specified folder.
+    /// </summary>
+    private class DirectoryTemplateLoader : ITemplateLoader {
+
+        public string? TemplateDirectory;
+
+
+        public string GetPath(TemplateContext context, SourceSpan callerSpan, string templateName) {
+            if (this.TemplateDirectory == null) {
+                return string.Empty;
+            }
+            
+            return Path.Combine(this.TemplateDirectory, templateName);
+        }
+
+
+        public string Load(TemplateContext context, SourceSpan callerSpan, string templatePath) {
+            return File.ReadAllText(templatePath);
+        }
+
+
+        public ValueTask<string> LoadAsync(TemplateContext context, SourceSpan callerSpan, string templatePath) {
+            return new ValueTask<string>(File.ReadAllText(templatePath));
+        }
+
+    }
+    
+}
